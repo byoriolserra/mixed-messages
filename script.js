949c15d8626a264987d02f560e6255324c3b0769
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2021-07-17 10:44:38 +0200

    First commit & write ideas string pieces

---
+++ script.js
@@ -0,0 +1,15 @@
+// This project will be an Art Piece Random Idea Generator!
+
+// It will have 3 string pieces that make up the final idea
+
+
+const Ideas = {
+    medium: ['movie', 'painting', 'song', 'statue', 'poem', 'novel', 'mixed media', 'play', 'video game'],
+    topic: ['the future of cities', 'the human nature', 'senses interpolation', 'the year 2077', 'your loveliest memory', 'a difficulty you\'re going through'],
+    method: ['in only 30 days', 'with a technique you\'ve never tried before', 'in only 24 hours starting NOW', 'with the help of another artist friend', 'as an open collaborative project'],
+
+};
+
+
+// Then the message will be created!
+
