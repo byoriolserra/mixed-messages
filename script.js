cce6a0cd8adb89cd8ff6b282c56c62b6b722c6e5
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2021-07-17 11:05:33 +0200

    Add messageFactory to create message

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // It will have 3 string pieces that make up the final idea
 
 
-const Ideas = {
-    medium: ['movie', 'painting', 'song', 'statue', 'poem', 'novel', 'mixed media', 'play', 'video game'],
+const ideas = {
+    medium: ['movie', 'painting', 'song', 'statue', 'poem', 'novel', 'mixed media piece', 'play', 'video game'],
     topic: ['the future of cities', 'the human nature', 'senses interpolation', 'the year 2077', 'your loveliest memory', 'a difficulty you\'re going through'],
     method: ['in only 30 days', 'with a technique you\'ve never tried before', 'in only 24 hours starting NOW', 'with the help of another artist friend', 'as an open collaborative project'],
 
@@ -13,3 +13,13 @@ const Ideas = {
 
 // Then the message will be created!
 
+const messageFactory = () => {
+    let pickedMedium = ideas.medium[Math.floor(Math.random() * ideas.medium.length)];
+    let pickedTopic = ideas.topic[Math.floor(Math.random() * ideas.topic.length)];
+    let pickedMethod = ideas.method[Math.floor(Math.random() * ideas.method.length)];
+
+    console.log(`Here's an Art Piece Idea: Why don't you create a ${pickedMedium} about ${pickedTopic} ${pickedMethod}? `);
+
+};
+
+messageFactory();
